340750b1b0513240bf872c2446e3e81fbe937fee
Jason Tseng <jsntseng14@gmail.com>
2021-09-06 22:03:37 -0700

    started on contact component

--- my-app/src/components/contact/Contact.jsx
+++ my-app/src/components/contact/Contact.jsx
@@ -4,7 +4,17 @@ import './contact.css'
 export default function contact() {
   return (
     <div className='contact' id='contact'>
-
+      <div className='left'>
+        <img src='assets/shake.svg' alt=''/>
+      </div>
+      <div className='right'>
+        <h2>Contact.</h2>
+        <form>
+          <input type='text' placeholder='Email'/>
+          <textarea placeholder ='Message'></textarea>
+          <button type ='submit'>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
