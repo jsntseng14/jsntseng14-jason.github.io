ae1a12bc1c67e1c9ea6132c6e4a7efe2394c1330
Jason Tseng <jsntseng14@gmail.com>
2021-09-09 23:44:59 -0700

    added animations to quote component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -16,8 +16,8 @@ function App() {
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className='sections'>
-        <Intro/>
         <Quote/>
+        <Intro/>
         <Portfolio/>
         <Works/>
         <Testimonials/>
