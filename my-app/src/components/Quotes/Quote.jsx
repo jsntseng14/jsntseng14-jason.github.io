024a40fdc728b70012d3726990795e2527a428ae
Jason Tseng <jsntseng14@gmail.com>
2021-09-09 23:45:51 -0700

    fixed a few animations with quote component

--- my-app/src/components/Quotes/Quote.jsx
+++ my-app/src/components/Quotes/Quote.jsx
@@ -4,18 +4,15 @@ import {TweenMax, Power3, gsap} from 'gsap'
 
 export default function Quote() {
   let item = useRef(null);
-  console.log(item);
 
   useEffect(() => {
-    console.log(item)
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+    tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+    tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+    tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+    tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+    tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
   },[])
 
