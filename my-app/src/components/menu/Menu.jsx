56d23516f7708544d2ee2cc887e7aa48b22e92ff
Jason Tseng <jsntseng14@gmail.com>
2021-09-22 21:44:51 -0700

    made some changes to index.html

--- my-app/src/components/menu/Menu.jsx
+++ my-app/src/components/menu/Menu.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './menu.scss'
 
 export default function Menu({ menuOpen, setMenuOpen}) {
+
   return (
+
     <div className={'menu ' + (menuOpen && 'active')}>
       <ul>
         <li onClick={()=> setMenuOpen(false)}>
