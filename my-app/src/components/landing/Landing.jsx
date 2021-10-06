a74b7ac438578cf73044a5fe5089bf8b198fa9c4
Jason Tseng <jsntseng14@gmail.com>
2021-10-05 21:52:44 -0700

    fixed some code and did cleanup

--- my-app/src/components/landing/Landing.jsx
+++ my-app/src/components/landing/Landing.jsx
@@ -1,5 +1,5 @@
 import { useRef, React, useEffect } from 'react'
-import {TweenMax, Power3, gsap} from 'gsap'
+import {TweenMax, gsap} from 'gsap'
 import './landing.scss'
 
 export default function Landing() {
